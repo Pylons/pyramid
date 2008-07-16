05f413bb4c3722aadd7c880a7550a229469b1075
Chris McDonough <chrism@agendaless.com>
2008-07-16 22:41:19 +0000

    Sigh.

--- repoze/bfg/__init__.py
+++ repoze/bfg/__init__.py
@@ -1,1 +1,6 @@
-from repoze.bfg.router import make_app # for import elsewhere
+try:
+    from repoze.bfg.router import make_app # for import elsewhere
+except ImportError:
+    # don't try so hard that we cause setup.py test to fail when the
+    # right modules aren't installed.
+    pass
