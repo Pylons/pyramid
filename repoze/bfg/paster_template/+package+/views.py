8d4d19927630b184111375dff853b4ba7014d651
Chris McDonough <chrism@agendaless.com>
2008-07-19 02:17:53 +0000

    Yup.

--- repoze/bfg/paster_template/+package+/views.py
+++ repoze/bfg/paster_template/+package+/views.py
@@ -1,5 +1,5 @@
-from repoze.bfg.template import render_transform_to_response
+from repoze.bfg.template import render_template_to_response
 
 def my_view(context, request):
-    return render_transform_to_response('templates/mymtemplate.pt',
-                                        greeting = 'hello world')
+    return render_template_to_response('templates/mymtemplate.pt',
+                                       greeting = 'hello world')
