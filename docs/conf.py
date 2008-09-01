691303e24513800bc490358c6e947e23035fa250
Chris McDonough <chrism@agendaless.com>
2008-09-01 16:57:06 +0000

    Prep for 0.3.5.

--- docs/conf.py
+++ docs/conf.py
@@ -51,9 +51,9 @@ copyright = '2008, Agendaless Consulting'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.3.4'
+version = '0.3.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.3.4'
+release = '0.3.5'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
