fbbe23f2e9b8b73bb68e45325b143ebf253a0be3
Chris McDonough <chrism@agendaless.com>
2008-10-02 22:51:34 +0000

    Fix failing class_implements test, add object implements test.

--- repoze/bfg/tests/test_events.py
+++ repoze/bfg/tests/test_events.py
@@ -53,13 +53,20 @@ class NewResponseEventTests(unittest.TestCase):
         self.assertEqual(inst.response, response)
 
 class WSGIAppEventTests(unittest.TestCase):
-    def test_class_implements(self):
+    def test_object_implements(self):
         from repoze.bfg.events import WSGIApplicationCreatedEvent
         event = WSGIApplicationCreatedEvent(object())
         from repoze.bfg.interfaces import IWSGIApplicationCreatedEvent
+        from zope.interface.verify import verifyObject
+        verifyObject(IWSGIApplicationCreatedEvent, event)
+
+    def test_class_implements(self):
+        from repoze.bfg.events import WSGIApplicationCreatedEvent
+        from repoze.bfg.interfaces import IWSGIApplicationCreatedEvent
         from zope.interface.verify import verifyClass
-        verifyClass(IWSGIApplicationCreatedEvent, event)
-        
+        verifyClass(IWSGIApplicationCreatedEvent, WSGIApplicationCreatedEvent)
+
+
 class DummyRequest:
     pass
 
