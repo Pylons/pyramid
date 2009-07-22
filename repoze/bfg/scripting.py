8d222e48b669652a2c454047cf821439f10c2a8c
Chris Rossi <chris@christophermrossi.com>
2009-07-22 16:04:55 +0000

    Document close on '__del__' behavior of closer.

--- repoze/bfg/scripting.py
+++ repoze/bfg/scripting.py
@@ -4,10 +4,12 @@ def get_root(app, environ=None):
     The ``root`` returned is the application root object.  The
     ``closer`` returned is a callable (accepting no arguments) that
     should be called when your scripting application is finished using
-    the root.  If ``environ`` is not None, it is used as the
-    environment passed to the BFG application root factory.  An empty
-    environ is constructed and passed to the root factory if
-    ``environ`` is None."""
+    the root.  The closer also closes the db connection when its ``__del__``
+    method is called.  This means the connection can also be closed by 
+    explicitly deleting the closer using ``del`` or, more commonly,  letting 
+    it fall out of scope.  If ``environ`` is not None, it is used as the 
+    environment passed to the BFG application root factory.  An empty environ 
+    is constructed and passed to the root factory if ``environ`` is None."""
     registry = app.registry
     threadlocals = {'registry':registry, 'request':None}
     app.threadlocal_manager.push(threadlocals)
