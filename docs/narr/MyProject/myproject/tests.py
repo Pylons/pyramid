5cf4f2a9b943e77a2ed59edc8e2ebf3ebde2f2ba
Chris McDonough <chrism@agendaless.com>
2009-10-09 18:54:55 +0000

    Remove comment.

--- docs/narr/MyProject/myproject/tests.py
+++ docs/narr/MyProject/myproject/tests.py
@@ -3,10 +3,6 @@ import unittest
 from repoze.bfg import testing
 
 class ViewTests(unittest.TestCase):
-    """ These tests are unit tests for the view.  They test the
-    functionality of *only* the view.  They register and use dummy
-    implementations of repoze.bfg functionality to allow you to avoid
-    testing 'too much'"""
     def test_my_view(self):
         from myproject.views import my_view
         context = testing.DummyModel()
