f89fb6cf111405e34808fc2933dd1fb8073ccd89
Chris McDonough <chrism@agendaless.com>
2009-10-09 18:54:12 +0000

    Use renderers.

--- docs/narr/MyProject/myproject/views.py
+++ docs/narr/MyProject/myproject/views.py
@@ -1,6 +1,2 @@
-from repoze.bfg.chameleon_zpt import render_template_to_response
-
 def my_view(context, request):
-    return render_template_to_response('templates/mytemplate.pt',
-                                       request = request,
-                                       project = 'MyProject')
+    return {'project':'MyProject'}
