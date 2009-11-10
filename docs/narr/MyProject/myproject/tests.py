1705a51195a6e54c242c6339996ff48501219223
Chris McDonough <chrism@agendaless.com>
2009-11-10 05:11:26 +0000

    More setup and teardown.

--- docs/narr/MyProject/myproject/tests.py
+++ docs/narr/MyProject/myproject/tests.py
@@ -3,6 +3,12 @@ import unittest
 from repoze.bfg import testing
 
 class ViewTests(unittest.TestCase):
+    def setUp(self):
+        testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
     def test_my_view(self):
         from myproject.views import my_view
         context = testing.DummyModel()
