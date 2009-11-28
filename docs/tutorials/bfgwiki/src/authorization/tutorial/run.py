7c4ee90fb28dcc352dbc8b5e6ffc31d27c02e947
Chris McDonough <chrism@agendaless.com>
2009-11-28 05:16:50 +0000

    Updates.

--- docs/tutorials/bfgwiki/src/authorization/tutorial/run.py
+++ docs/tutorials/bfgwiki/src/authorization/tutorial/run.py
@@ -1,20 +1,20 @@
-from repoze.bfg.router import make_app
+from repoze.bfg.configuration import Configurator
 from repoze.zodbconn.finder import PersistentApplicationFinder
 
+from tutorial.models import appmaker
 
 def app(global_config, **settings):
-    """ This function returns a repoze.bfg.router.Router object.
+    """ This function returns a ``repoze.bfg`` WSGI application.
     
     It is usually called by the PasteDeploy framework during ``paster serve``.
     """
-    # paster app config callback
-    import tutorial
-    from tutorial.models import appmaker
     zodb_uri = settings.get('zodb_uri')
     if zodb_uri is None:
         raise ValueError("No 'zodb_uri' in application configuration.")
     finder = PersistentApplicationFinder(zodb_uri, appmaker)
     def get_root(request):
         return finder(request.environ)
-    return make_app(get_root, tutorial, settings=settings)
+    config = Configurator(root_factory=get_root, settings=settings)
+    config.load_zcml('configure.zcml')
+    return config.make_wsgi_app()
 
--- docs/tutorials/bfgwiki/src/basiclayout/setup.py
+++ docs/tutorials/bfgwiki/src/basiclayout/setup.py
@@ -6,39 +6,37 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
+requires = [
+    'repoze.bfg',
+    'docutils',
+    'ZODB3',
+    'repoze.zodbconn',
+    'repoze.tm',
+    ]
+
 setup(name='tutorial',
-      version='0.1',
+      version='0.0',
       description='tutorial',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
-        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "Framework :: BFG",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
       author='',
       author_email='',
       url='',
-      keywords='web wsgi bfg zope',
+      keywords='web wsgi bfg',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-            'repoze.bfg',
-            'repoze.zodbconn',
-            'repoze.tm',
-            'ZODB3',
-            ],
-      tests_require=[
-            'repoze.bfg',
-            ],
+      install_requires=requires,
+      tests_require=requires,
       test_suite="tutorial",
       entry_points = """\
       [paste.app_factory]
       app = tutorial.run:app
       """
       )
-
