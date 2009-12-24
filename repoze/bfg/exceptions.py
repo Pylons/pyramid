18e5d6c8fe922bf23ccbd481d60de294d7671103
Chris McDonough <chrism@agendaless.com>
2009-12-24 00:56:35 +0000

    Roles.

--- repoze/bfg/exceptions.py
+++ repoze/bfg/exceptions.py
@@ -3,9 +3,9 @@ from zope.configuration.exceptions import ConfigurationError as ZCE
 class Forbidden(Exception):
     """\
     Raise this exception within :term:`view` code to immediately
-    return the Forbidden view to the invoking user.  Usually this is a
-    basic ``401`` page, but the Forbidden view can be customized as
-    necessary.  See :ref:`changing_the_forbidden_view`.
+    return the :term:`forbidden view` to the invoking user.  Usually
+    this is a basic ``401`` page, but the forbidden view can be
+    customized as necessary.  See :ref:`changing_the_forbidden_view`.
 
     This exception's constructor accepts a single positional argument,
     which should be a string.  The value of this string will be placed
@@ -15,9 +15,9 @@ class Forbidden(Exception):
 class NotFound(Exception):
     """\
     Raise this exception within :term:`view` code to immediately
-    return the Not Found view to the invoking user.  Usually this is a
-    basic ``404`` page, but the Not Found view can be customized as
-    necessary.  See :ref:`changing_the_notfound_view`.
+    return the :term:`Not Found view` to the invoking user.  Usually
+    this is a basic ``404`` page, but the Not Found view can be
+    customized as necessary.  See :ref:`changing_the_notfound_view`.
 
     This exception's constructor accepts a single positional argument,
     which should be a string.  The value of this string will be placed
