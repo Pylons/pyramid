67bfd86e2be46a459468e9e7d33c49e0629d178d
Chris McDonough <chrism@agendaless.com>
2009-12-25 04:30:10 +0000

    Make latex/pdf output work better.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'repoze.bfg'
-copyright = '2008-2009, Agendaless Consulting'
+copyright = '2008-2010, Agendaless Consulting'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -103,7 +103,7 @@ html_style = 'repoze.css'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-html_logo = '.static/logo_hi.gif'
+html_logo = '.static/logo_hi.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -158,24 +158,24 @@ htmlhelp_basename = 'repozebfgdoc'
 # ------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'letter'
 
 # The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
+latex_font_size = '10pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'repozebfg.tex', 'repoze.bfg Documentation', 'Agendaless Consulting', 'manual'),
+  ('index', 'repozebfg.tex', 'repoze.bfg Documentation', 'Chris McDonough', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '.static/logo_hi.gif'
+latex_logo = '.static/logo_hi.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = True
 
 # Additional stuff for the LaTeX preamble.
 #latex_preamble = ''
@@ -184,4 +184,4 @@ latex_logo = '.static/logo_hi.gif'
 #latex_appendices = []
 
 # If false, no module index is generated.
-#latex_use_modindex = True
+latex_use_modindex = True
