a0169ff21b5cd7446bf0289bffdb52de8dc61290
Chris McDonough <chrism@agendaless.com>
2009-12-25 04:43:38 +0000

    Format.

--- docs/conf.py
+++ docs/conf.py
@@ -166,7 +166,8 @@ latex_font_size = '10pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'repozebfg.tex', 'repoze.bfg Documentation', 'Chris McDonough', 'manual'),
+  ('index', 'repozebfg.tex', 'repoze.bfg Documentation', 'Chris McDonough',
+   'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
