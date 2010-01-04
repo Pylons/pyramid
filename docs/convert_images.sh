d5f0bf1d94a11c0a787260be2130183d5942f63e
Chris McDonough <chrism@agendaless.com>
2010-01-04 13:44:58 +0000

    Prep for 1.2a10.

--- docs/convert_images.sh
+++ docs/convert_images.sh
@@ -5,6 +5,7 @@ if test ! -z $BOOK; then
   do
       cp $img ${img}.BAK
       convert $img -units PixelsPerInch -resample 300 -colorspace Gray ${img}.grey
+      #convert -strip -density 300 ${img} -units PixelsPerInch -resample 300 -colorspace Gray ${img}.grey
       mv ${img}.grey $img
   done
 fi
