23ab84757a4d216a7c5eea77e85aedfd3a9400ca
Chris McDonough <chrism@agendaless.com>
2010-06-30 17:24:23 +0000

    pass only match and route and document route minimally

--- repoze/bfg/urldispatch.py
+++ repoze/bfg/urldispatch.py
@@ -51,7 +51,10 @@ class RoutesMapper(object):
             match = route.match(path)
             if match is not None:
                 preds = route.predicates
-                info = {'route':route, 'match':match}
+                # NB: it is the intent that only 'match' be relied on
+                # by built-in predicates.  'route' and 'mapper' may be
+                # used by custom predicates.
+                info = {'match':match, 'route':route}
                 if preds and not all((p(info, request) for p in preds)):
                     continue
                 return info
