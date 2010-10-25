c426c49cd433997aac10045052d77d9c749680b2
Chris McDonough <chrism@plope.com>
2010-10-25 17:58:23 -0400

    remove bfgwiki2 old dir

--- docs/tutorials/bfgwiki2/src/basiclayout/tutorial/views.py
+++
@@ -1,7 +0,0 @@
-from tutorial.models import DBSession
-from tutorial.models import MyModel
-
-def my_view(request):
-    dbsession = DBSession()
-    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
-    return {'root':root, 'project':'tutorial'}
