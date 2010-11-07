7e719ff9950a7db2faa6b2361fe7151aa8507555
Xavier Spriet <linkadmin@gmail.com>
2010-11-07 10:29:56 -0800

    * Use edit.pt instead of view.pt for the add_page view callable in the wiki tutorial.

--- docs/tutorials/wiki2/src/views/tutorial/__init__.py
+++ docs/tutorials/wiki2/src/views/tutorial/__init__.py
@@ -23,7 +23,7 @@ def app(global_config, **settings):
                      view_renderer='tutorial:templates/view.pt')
     config.add_route('add_page', '/add_page/:pagename',
                      view='tutorial.views.add_page',
-                     view_renderer='tutorial:templates/view.pt')
+                     view_renderer='tutorial:templates/edit.pt')
     config.add_route('edit_page', '/:pagename/edit_page',
                      view='tutorial.views.edit_page',
                      view_renderer='tutorial:templates/edit.pt')
