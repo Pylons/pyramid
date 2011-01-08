a063aff1b0a9fe1f9d8c4a4baeade6453695f171
Chris McDonough <chrism@plope.com>
2011-01-08 00:46:32 -0500

    prep for 1.0a9

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ copyright = '%s, Agendaless Consulting' % datetime.datetime.now().year
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.0a8'
+version = '1.0a9'
 # The full version, including alpha/beta/rc tags.
 release = version
 
