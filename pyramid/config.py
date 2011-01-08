512d53fffab8f0d258f528d0c24c313b65bde652
Chris McDonough <chrism@plope.com>
2011-01-07 22:29:15 -0500

    comment about sphinx

--- pyramid/config.py
+++ pyramid/config.py
@@ -125,7 +125,7 @@ def action_method(wrapped):
         return result
     wrapper.__name__ = wrapped.__name__
     wrapper.__doc__ = wrapped.__doc__
-    wrapper.__docobj__ = wrapped
+    wrapper.__docobj__ = wrapped # for sphinx
     return wrapper
 
 class Configurator(object):
