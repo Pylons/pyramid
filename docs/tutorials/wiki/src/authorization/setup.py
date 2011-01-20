0d7e95b06e8946247e04552a1fb395ab107155eb
Chris McDonough <chrism@plope.com>
2011-01-19 21:44:30 -0500

    adjust tutorial sources against pyramid_zodb changes

--- docs/tutorials/wiki/src/authorization/setup.py
+++ docs/tutorials/wiki/src/authorization/setup.py
@@ -9,10 +9,9 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'repoze.zodbconn',
-    'repoze.tm',
+    'repoze.tm2>=1.0b1', # default_commit_veto
     'ZODB3',
     'WebError',
-    'docutils',
     ]
 
 setup(name='tutorial',
