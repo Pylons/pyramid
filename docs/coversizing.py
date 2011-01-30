d463038860c29c8a80db92f2ffdfe804260d6912
Chris McDonough <chrism@plope.com>
2011-01-30 14:10:06 -0500

    add cover sizing script

---
+++ docs/coversizing.py
@@ -0,0 +1,15 @@
+# see https://www.createspace.com/Products/Book/#content4
+
+page_count = 605
+bleed = .125
+spine_width = .002252 * page_count
+trim_width = 7.5
+trim_height = 9.25
+min_cover_width = bleed + trim_width + spine_width + trim_width + bleed
+min_cover_height = bleed + trim_height + bleed
+
+print "spine width ", spine_width
+print "min cover width ", min_cover_width
+print "min cover height ", min_cover_height
+
+
