b8fc93e83a646adbe83781feccc1d106310a79f2
Chris McDonough <chrism@plope.com>
2011-02-17 01:54:04 -0500

    Undo the inner squiggly regex fix introduced in commit #9595236 because the regex breaks on Jython.  See https://github.com/Pylons/pyramid/issues/#issue/123

--- pyramid/urldispatch.py
+++ pyramid/urldispatch.py
@@ -76,12 +76,21 @@ class RoutesMapper(object):
 # stolen from bobo and modified
 old_route_re = re.compile(r'(\:[a-zA-Z]\w*)')
 star_in_brackets = re.compile(r'\{[^\}]*\*\w*[^\}]*\}')
-# The torturous nature of the regex named ``route_re`` below is due to the
-# fact that we need to support at least one level of "inner" squigglies
-# inside the expr of a {name:expr} pattern.  This regex used to be just
-# (\{[a-zA-Z][^\}]*\}) but that choked when supplied with e.g. {foo:\d{4}}.
-# Thanks to Zart for the regex help.
-route_re = re.compile(r'(\{[a-zA-Z](?:\{[^\}]*\}|[^\{\}]*)*\})')
+
+# The regex named ``torturous_route_re`` below allows us to support at least
+# one level of "inner" squigglies inside the expr of a {name:expr} pattern;
+# for example, {foo:\d{4}}.  Thanks to Zart for the regex help.
+# ``torturous_route_re`` is meant to be a replacement for the regex named
+# ``route_re`` (which is just (\{[a-zA-Z][^\}]*\})) because ``route_re``
+# chokes when it encounters inner squigglies.  However
+# http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5050507 means that the
+# torturous regex doesn't work on Jython (recursion error), so we've disabled
+# it in favor of ``route_re`` for now.  If somebody can make something that
+# will work on Jython but also match inner squigglies, it'd be useful.
+
+# torturous_route_re = re.compile(r'(\{[a-zA-Z](?:\{[^\}]*\}|[^\{\}]*)*\})')
+
+route_re = re.compile(r'(\{[a-zA-Z][^\}]*\})')
 
 def update_pattern(matchobj):
     name = matchobj.group(0)
