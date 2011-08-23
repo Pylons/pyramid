30d74ad4b0fb8484d0265f429b87c7a88bee4192
Chris McDonough <chrism@plope.com>
2011-08-22 21:42:07 -0400

    break security-related method tests out of test_init

---
+++ pyramid/tests/test_config/test_security.py
@@ -0,0 +1,83 @@
+import unittest
+
+class ConfiguratorSecurityMethodsTests(unittest.TestCase):
+    def _makeOne(self, *arg, **kw):
+        from pyramid.config import Configurator
+        config = Configurator(*arg, **kw)
+        return config
+
+    def test_set_authentication_policy_no_authz_policy(self):
+        from zope.configuration.config import ConfigurationExecutionError
+        config = self._makeOne()
+        policy = object()
+        config.set_authentication_policy(policy)
+        self.assertRaises(ConfigurationExecutionError, config.commit)
+
+    def test_set_authentication_policy_no_authz_policy_autocommit(self):
+        from pyramid.interfaces import IAuthenticationPolicy
+        config = self._makeOne(autocommit=True)
+        policy = object()
+        config.set_authentication_policy(policy)
+        self.assertEqual(
+            config.registry.getUtility(IAuthenticationPolicy), policy)
+
+    def test_set_authentication_policy_with_authz_policy(self):
+        from pyramid.interfaces import IAuthenticationPolicy
+        from pyramid.interfaces import IAuthorizationPolicy
+        config = self._makeOne()
+        authn_policy = object()
+        authz_policy = object()
+        config.registry.registerUtility(authz_policy, IAuthorizationPolicy)
+        config.set_authentication_policy(authn_policy)
+        config.commit()
+        self.assertEqual(
+            config.registry.getUtility(IAuthenticationPolicy), authn_policy)
+
+    def test_set_authentication_policy_with_authz_policy_autocommit(self):
+        from pyramid.interfaces import IAuthenticationPolicy
+        from pyramid.interfaces import IAuthorizationPolicy
+        config = self._makeOne(autocommit=True)
+        authn_policy = object()
+        authz_policy = object()
+        config.registry.registerUtility(authz_policy, IAuthorizationPolicy)
+        config.set_authentication_policy(authn_policy)
+        config.commit()
+        self.assertEqual(
+            config.registry.getUtility(IAuthenticationPolicy), authn_policy)
+
+    def test_set_authorization_policy_no_authn_policy(self):
+        from zope.configuration.config import ConfigurationExecutionError
+        config = self._makeOne()
+        policy = object()
+        config.set_authorization_policy(policy)
+        self.assertRaises(ConfigurationExecutionError, config.commit)
+
+    def test_set_authorization_policy_no_authn_policy_autocommit(self):
+        from pyramid.exceptions import ConfigurationError
+        config = self._makeOne(autocommit=True)
+        policy = object()
+        self.assertRaises(ConfigurationError,
+                          config.set_authorization_policy, policy)
+
+    def test_set_authorization_policy_with_authn_policy(self):
+        from pyramid.interfaces import IAuthorizationPolicy
+        from pyramid.interfaces import IAuthenticationPolicy
+        config = self._makeOne()
+        authn_policy = object()
+        authz_policy = object()
+        config.registry.registerUtility(authn_policy, IAuthenticationPolicy)
+        config.set_authorization_policy(authz_policy)
+        self.assertEqual(
+            config.registry.getUtility(IAuthorizationPolicy), authz_policy)
+
+    def test_set_authorization_policy_with_authn_policy_autocommit(self):
+        from pyramid.interfaces import IAuthorizationPolicy
+        from pyramid.interfaces import IAuthenticationPolicy
+        config = self._makeOne(autocommit=True)
+        authn_policy = object()
+        authz_policy = object()
+        config.registry.registerUtility(authn_policy, IAuthenticationPolicy)
+        config.set_authorization_policy(authz_policy)
+        self.assertEqual(
+            config.registry.getUtility(IAuthorizationPolicy), authz_policy)
+
