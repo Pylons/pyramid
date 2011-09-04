d295cdda42f8b2e91db42fd5fd9cd6b3469aea82
Chris McDonough <chrism@plope.com>
2011-09-04 02:18:22 -0400

    remove lies from docstring

--- pyramid/config/zca.py
+++ pyramid/config/zca.py
@@ -3,22 +3,17 @@ from zope.component import getSiteManager
 
 class ZCAConfiguratorMixin(object):
     def hook_zca(self):
-        """ Call :func:`zope.component.getSiteManager.sethook` with
-        the argument
-        :data:`pyramid.threadlocal.get_current_registry`, causing
+        """ Call :func:`zope.component.getSiteManager.sethook` with the
+        argument :data:`pyramid.threadlocal.get_current_registry`, causing
         the :term:`Zope Component Architecture` 'global' APIs such as
         :func:`zope.component.getSiteManager`,
         :func:`zope.component.getAdapter` and others to use the
-        :app:`Pyramid` :term:`application registry` rather than the
-        Zope 'global' registry.  If :mod:`zope.component` cannot be
-        imported, this method will raise an :exc:`ImportError`."""
+        :app:`Pyramid` :term:`application registry` rather than the Zope
+        'global' registry."""
         getSiteManager.sethook(get_current_registry)
 
     def unhook_zca(self):
-        """ Call :func:`zope.component.getSiteManager.reset` to undo
-        the action of
-        :meth:`pyramid.config.Configurator.hook_zca`.  If
-        :mod:`zope.component` cannot be imported, this method will
-        raise an :exc:`ImportError`."""
+        """ Call :func:`zope.component.getSiteManager.reset` to undo the
+        action of :meth:`pyramid.config.Configurator.hook_zca`."""
         getSiteManager.reset()
 
