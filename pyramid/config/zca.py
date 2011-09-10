58bd32c7cdaff6d1b0e578e8a6887ea2f519866e
Chris McDonough <chrism@plope.com>
2011-09-10 18:08:22 -0400

    futureproof by importing at function scope

--- pyramid/config/zca.py
+++ pyramid/config/zca.py
@@ -1,5 +1,4 @@
 from pyramid.threadlocal import get_current_registry
-from zope.component import getSiteManager
 
 class ZCAConfiguratorMixin(object):
     def hook_zca(self):
@@ -10,10 +9,12 @@ class ZCAConfiguratorMixin(object):
         :func:`zope.component.getAdapter` and others to use the
         :app:`Pyramid` :term:`application registry` rather than the Zope
         'global' registry."""
+        from zope.component import getSiteManager
         getSiteManager.sethook(get_current_registry)
 
     def unhook_zca(self):
         """ Call :func:`zope.component.getSiteManager.reset` to undo the
         action of :meth:`pyramid.config.Configurator.hook_zca`."""
+        from zope.component import getSiteManager
         getSiteManager.reset()
 
