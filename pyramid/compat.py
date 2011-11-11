d68f850b513f0daad984d87c5ff0714753e7c815
Chris McDonough <chrism@plope.com>
2011-11-11 14:41:02 -0500

    readd json back, but not as an API (only because older versions of debug toolbar depend on its existence)

--- pyramid/compat.py
+++ pyramid/compat.py
@@ -208,3 +208,12 @@ try:
 except ImportError: # pragma: no cover
     from io import StringIO as NativeIO
     
+try:
+    import json
+except ImportError: # pragma: no cover
+    try:
+        import simplejson as json
+    except NotImplementedError:
+        from django.utils import simplejson as json # GAE
+
+    
