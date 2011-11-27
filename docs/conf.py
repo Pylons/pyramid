477f51b6e04e5b2e6c2d30ce8c9884eb08516e92
Blaise Laflamme <blaise@laflamme.org>
2011-11-27 12:46:11 -0500

    updated doc theme workflow

--- docs/conf.py
+++ docs/conf.py
@@ -141,9 +141,28 @@ if book:
 # -----------------------
 
 # Add and use Pylons theme
+from subprocess import call, Popen, PIPE
+
+p = Popen('which git', shell=True, stdout=PIPE)
+git = p.stdout.read().strip()
+cwd = os.getcwd()
+_themes = os.path.join(cwd, '_themes')
+
+if not os.path.isdir(_themes):
+    call(['git', 'clone', 'git://github.com/Pylons/pylons_sphinx_theme.git',
+            '_themes'])
+else:
+    os.chdir(_themes)
+    call(['git', 'checkout', 'master'])
+    call(['git', 'pull'])
+    os.chdir(cwd)
+
 sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
 html_theme = 'pyramid'
+html_theme_options = dict(
+    github_url='https://github.com/Pylons/pyramid'
+    )
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
