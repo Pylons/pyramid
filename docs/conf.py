f460fb5ae324126ae1364dfefa0edd99a62428ba
Blaise Laflamme <blaise@laflamme.org>
2011-11-27 13:15:49 -0500

    fixed git ref

--- docs/conf.py
+++ docs/conf.py
@@ -149,12 +149,12 @@ cwd = os.getcwd()
 _themes = os.path.join(cwd, '_themes')
 
 if not os.path.isdir(_themes):
-    call(['git', 'clone', 'git://github.com/Pylons/pylons_sphinx_theme.git',
+    call([git, 'clone', 'git://github.com/Pylons/pylons_sphinx_theme.git',
             '_themes'])
 else:
     os.chdir(_themes)
-    call(['git', 'checkout', 'master'])
-    call(['git', 'pull'])
+    call([git, 'checkout', 'master'])
+    call([git, 'pull'])
     os.chdir(cwd)
 
 sys.path.append(os.path.abspath('_themes'))
