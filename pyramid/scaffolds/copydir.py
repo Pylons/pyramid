74e69aa12483325039ad64d9b7494ed20ddcb2b1
Chris McDonough <chrism@plope.com>
2011-12-05 00:23:39 -0500

    cant mix mapping-style and tuple-style formatting in a format string; fixes #366

--- pyramid/scaffolds/copydir.py
+++ pyramid/scaffolds/copydir.py
@@ -161,8 +161,7 @@ def should_skip_file(name):
     if name.endswith('~') or name.endswith('.bak'):
         return 'Skipping backup file %(filename)s'
     if name.endswith('.pyc') or name.endswith('.pyo'):
-        return 'Skipping %s file %(filename)s' % os.path.splitext(name)[1]
-        # return 'Skipping %s file ' % os.path.splitext(name)[1] + '%(filename)s'
+        return 'Skipping %s file ' % os.path.splitext(name)[1] + '%(filename)s'
     if name.endswith('$py.class'):
         return 'Skipping $py.class file %(filename)s'
     if name in ('CVS', '_darcs'):
--- pyramid/tests/test_scaffolds/test_copydir.py
+++ pyramid/tests/test_scaffolds/test_copydir.py
@@ -237,11 +237,10 @@ class Test_should_skip_file(unittest.TestCase):
 
     def test_should_skip_bytecompiled_file(self):
         for name in ('afilename.pyc', 'afilename.pyo'):
-            # extension = os.path.splitext(name)[1]
-            # self.assertEqual(
-            #     self._callFUT(name),
-            #     'Skipping %s file ' % extension + '%(filename)s')
-            self.assertRaises(TypeError, self._callFUT, name)
+            extension = os.path.splitext(name)[1]
+            self.assertEqual(
+                self._callFUT(name),
+                'Skipping %s file ' % extension + '%(filename)s')
 
     def test_should_skip_jython_class_file(self):
         self.assertEqual(
