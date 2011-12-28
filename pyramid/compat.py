5a3ddb76a7ac4ee6f41652f774e8387a8c9b131a
Chris McDonough <chrism@plope.com>
2011-12-28 09:28:18 -0500

    typo

--- pyramid/compat.py
+++ pyramid/compat.py
@@ -33,7 +33,7 @@ else:
 
 def text_(s, encoding='latin-1', errors='strict'):
     """ If ``s`` is an instance of ``binary_type``, return
-    ``s.encode(encoding, errors)``, otherwise return ``s``"""
+    ``s.decode(encoding, errors)``, otherwise return ``s``"""
     if isinstance(s, binary_type):
         return s.decode(encoding, errors)
     return s # pragma: no cover
