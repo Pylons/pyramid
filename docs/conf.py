cf3a11e990adda800e284effb006f1c28335da4d
Chris McDonough <chrism@plope.com>
2012-01-09 03:49:46 -0500

    prep for 1.3a5

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ copyright = '%s, Agendaless Consulting' % datetime.datetime.now().year
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.3a4'
+version = '1.3a5'
 
 # The full version, including alpha/beta/rc tags.
 release = version
--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if not PY3:
         ])
 
 setup(name='pyramid',
-      version='1.3a4',
+      version='1.3a5',
       description=('The Pyramid web application development framework, a '
                    'Pylons project'),
       long_description=README + '\n\n' +  CHANGES,
