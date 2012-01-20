05f462e9792d9b5f6560968503795dc162984408
Chris McDonough <chrism@plope.com>
2012-01-19 20:36:45 -0500

    Backport fix for issue #407 to 1.3 branch.

--- pyramid/config/views.py
+++ pyramid/config/views.py
@@ -1545,11 +1545,12 @@ class StaticURLInfo(object):
             registry = get_current_registry()
         for (url, spec, route_name) in self._get_registrations(registry):
             if path.startswith(spec):
-                subpath = url_quote(path[len(spec):])
+                subpath = path[len(spec):]
                 if url is None:
                     kw['subpath'] = subpath
                     return request.route_url(route_name, **kw)
                 else:
+                    subpath = url_quote(subpath)
                     return urljoin(url, subpath)
 
         raise ValueError('No static URL definition matching %s' % path)
