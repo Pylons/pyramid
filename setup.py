662d6ea96a79cdd9943a9e3c871fe07282ee671c
Chris McDonough <chrism@plope.com>
2012-02-22 21:10:08 -0500

    prep for 1.3a9

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if not PY3:
         ])
 
 setup(name='pyramid',
-      version='1.3a8',
+      version='1.3a9',
       description=('The Pyramid web application development framework, a '
                    'Pylons project'),
       long_description=README + '\n\n' +  CHANGES,
