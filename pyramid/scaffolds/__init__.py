9cdabcf16c2487a5a5188ab06b7ab5b89ba12bd9
Michael Merickel <michael@merickel.org>
2012-03-13 15:47:10 -0500

    automatically generate pyramid doc urls in scaffold ini files

--- pyramid/scaffolds/__init__.py
+++ pyramid/scaffolds/__init__.py
@@ -40,17 +40,65 @@ class PyramidTemplate(Template):
     def out(self, msg): # pragma: no cover (replaceable testing hook)
         print(msg)
 
+
+_DEBUGTOOLBAR_URL_FMT = ('http://docs.pylonsproject.org/projects/'
+                         'pyramid_debugtoolbar/en/%(version)s/#settings')
+_LOGGING_URL_FMT = ('http://docs.pylonsproject.org/projects/'
+                    'pyramid/en/%(version)s/narr/logging.html')
+_SETTINGS_URL_FMT = ('http://docs.pylonsproject.org/projects/'
+                     'pyramid/en/%(version)s/narr/environment.html')
+
+def _get_version(name, pkg_resources=None):
+    """ Get the version of a package or None."""
+    try:
+        if pkg_resources is None: # pragma: no cover
+            import pkg_resources
+        pkg = pkg_resources.get_distribution(name)
+        return pkg.parsed_version
+    except (ImportError, ValueError):
+        pass
+
+def _make_pylons_link(name, pkg_resources=None):
+    """ Create a link to the pyramid documentation."""
+    v = _get_version(name, pkg_resources=pkg_resources)
+    try:
+        return '%s.%s-branch' % (int(v[0]), int(v[1]))
+    except:
+        return 'latest'
+
+def _add_ini_urls(vars, pkg_resources=None):
+    """ Add some basic URLs to the template vars for use in INI comments."""
+    pyramid_version = _make_pylons_link('pyramid', pkg_resources=pkg_resources)
+    debugtoolbar_version = 'latest'
+    vars['settings_url'] = _SETTINGS_URL_FMT % {'version': pyramid_version}
+    vars['logging_url'] = _LOGGING_URL_FMT % {'version': pyramid_version}
+    vars['debugtoolbar_url'] = _DEBUGTOOLBAR_URL_FMT % {'version':
+                                                        debugtoolbar_version}
+
 class StarterProjectTemplate(PyramidTemplate):
     _template_dir = 'starter'
     summary = 'Pyramid starter project'
+
+    def pre(self, command, output_dir, vars):
+        _add_ini_urls(vars)
+        return PyramidTemplate.pre(self, command, output_dir, vars)
 
 class ZODBProjectTemplate(PyramidTemplate):
     _template_dir = 'zodb'
     summary = 'Pyramid ZODB project using traversal'
 
+    def pre(self, command, output_dir, vars):
+        _add_ini_urls(vars)
+        return PyramidTemplate.pre(self, command, output_dir, vars)
+
 class AlchemyProjectTemplate(PyramidTemplate):
     _template_dir = 'alchemy'
     summary = 'Pyramid SQLAlchemy project using url dispatch'
+
+    def pre(self, command, output_dir, vars):
+        _add_ini_urls(vars)
+        return PyramidTemplate.pre(self, command, output_dir, vars)
+
     def post(self, command, output_dir, vars): # pragma: no cover
         val = PyramidTemplate.post(self, command, output_dir, vars)
         vars = vars.copy()
--- pyramid/tests/test_scaffolds/test_init.py
+++ pyramid/tests/test_scaffolds/test_init.py
@@ -24,3 +24,54 @@ class TestPyramidTemplate(unittest.TestCase):
         self.assertTrue(vars['random_string'])
         self.assertEqual(vars['package_logger'], 'app')
 
+class TestINIURLs(unittest.TestCase):
+
+    def test_it(self):
+        from pyramid.scaffolds import _add_ini_urls
+        vars = {}
+        pkg_resources = DummyPkgResources({'pyramid': DummyDistribution()})
+        _add_ini_urls(vars, pkg_resources=pkg_resources)
+        self.assertEqual(
+            vars['settings_url'],
+            ('http://docs.pylonsproject.org/projects/'
+             'pyramid/en/1.3-branch/narr/environment.html'))
+        self.assertEqual(
+            vars['logging_url'],
+            ('http://docs.pylonsproject.org/projects/'
+             'pyramid/en/1.3-branch/narr/logging.html'))
+        self.assertEqual(
+            vars['debugtoolbar_url'],
+            ('http://docs.pylonsproject.org/projects/'
+             'pyramid_debugtoolbar/en/latest/#settings'))
+
+    def test_it_failover(self):
+        from pyramid.scaffolds import _add_ini_urls
+        vars = {}
+        class _dist(object):
+            @property
+            def parsed_version(self):
+                raise ValueError
+        pkg_resources = DummyPkgResources({'pyramid': _dist})
+        _add_ini_urls(vars, pkg_resources=pkg_resources)
+        self.assertEqual(
+            vars['settings_url'],
+            ('http://docs.pylonsproject.org/projects/'
+             'pyramid/en/latest/narr/environment.html'))
+        self.assertEqual(
+            vars['logging_url'],
+            ('http://docs.pylonsproject.org/projects/'
+             'pyramid/en/latest/narr/logging.html'))
+        self.assertEqual(
+            vars['debugtoolbar_url'],
+            ('http://docs.pylonsproject.org/projects/'
+             'pyramid_debugtoolbar/en/latest/#settings'))
+
+class DummyDistribution(object):
+    parsed_version = ('00000001', '00000003', '*b', '00000002', '*final')
+
+class DummyPkgResources(object):
+    def __init__(self, dists):
+        self.dists = dists
+
+    def get_distribution(self, name):
+        return self.dists[name]
