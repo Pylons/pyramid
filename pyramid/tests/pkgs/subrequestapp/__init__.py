5da21f623e0aa71106bd54bf0d199435f003b1d4
Chris McDonough <chrism@plope.com>
2012-09-16 04:06:37 -0400

    use a string renderer

--- pyramid/tests/pkgs/subrequestapp/__init__.py
+++ pyramid/tests/pkgs/subrequestapp/__init__.py
@@ -7,14 +7,13 @@ def view_one(request):
     return response
 
 def view_two(request):
-    request.response.body = b'This came from view_two'
-    return request.response
+    return 'This came from view_two'
 
 def main():
     config = Configurator()
     config.add_route('one', '/view_one')
     config.add_route('two', '/view_two')
     config.add_view(view_one, route_name='one')
-    config.add_view(view_two, route_name='two')
+    config.add_view(view_two, route_name='two', renderer='string')
     return config
 
