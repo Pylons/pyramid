60ed104358863c58166fe79bce06bec9d6b61c21
ericrasmussen <ericrasmussen@gmail.com>
2012-11-05 17:31:10 -0800

    adding transaction module requirement to zodb wiki docs

--- docs/tutorials/wiki/src/tests/setup.py
+++ docs/tutorials/wiki/src/tests/setup.py
@@ -9,6 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'pyramid_zodbconn',
+    'transaction',
     'pyramid_tm',
     'pyramid_debugtoolbar',
     'ZODB3',
