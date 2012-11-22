1608b26c9d3967d258613f038ff8b5f258a4a698
Chris McDonough <chrism@plope.com>
2012-11-21 23:19:43 -0500

    prep for 1.4b1

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ copyright = '%s, Agendaless Consulting' % datetime.datetime.now().year
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.4a4'
+version = '1.4b1'
 
 # The full version, including alpha/beta/rc tags.
 release = version
--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.4a4',
+      version='1.4b1',
       description=('The Pyramid web application development framework, a '
                    'Pylons project'),
       long_description=README + '\n\n' +  CHANGES,
