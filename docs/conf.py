Pylons/pyramid

commit 52d4fbc978f7244d12a82a43163651b962fb8861

make it unnecessary to change conf.py during a release, change author email, change version number to 0.0 until a release

docs/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default value.
 
 import sys, os
+import datetime
 
 # skip raw nodes
 from sphinx.writers.text import TextTranslator
@@ -36,6 +37,10 @@ for item in os.listdir(parent):
     if item.endswith('.egg'):
         sys.path.append(os.path.join(parent, item))
 
+import pkginfo
+
+pkg_info = pkginfo.Develop(os.path.join(os.path.dirname(__file__),'..'))
+
 # General configuration
 # ---------------------
 
@@ -55,13 +60,13 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'The Pyramid Web Application Development Framework'
-copyright = '2008-2010, Agendaless Consulting'
+copyright = '%s, Agendaless Consulting' % datetime.datetime.now().year
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.0a1'
+version = pkg_info.version
 # The full version, including alpha/beta/rc tags.
 release = version
 

setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '1.0a1'
+__version__ = '0.0'
 
 import os
 import platform
@@ -48,7 +48,8 @@ if platform.system() == 'Java':
     tests_require = install_requires + ['twill']
 else:
     tests_require= install_requires + ['Sphinx', 'docutils', 'coverage',
-                                       'twill', 'repoze.sphinx.autointerface']
+                                       'twill', 'repoze.sphinx.autointerface',
+                                       'pkginfo']
 
 if sys.version_info[:2] < (2, 6):
     install_requires.append('simplejson')
@@ -67,7 +68,7 @@ setup(name='pyramid',
         ],
       keywords='web wsgi pylons pyramid bfg',
       author="Chris McDonough, Agendaless Consulting",
-      author_email="repoze-dev@lists.repoze.org",
+      author_email="pylons-devel@googlegroups.com",
       url="http://pylonshq.com",
       license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
       packages=find_packages(),

commit 3604e6edbf7f9aac375ca3b2a9914c5665a0cf8d

Pyramid copyright is from 2008
52d4fbc mistakenly removed '2008' in '2008-<current year>'

docs/conf.py
@@ -76,7 +76,8 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'The Pyramid Web Application Development Framework'
-copyright = '%s, Agendaless Consulting' % datetime.datetime.now().year
+thisyear = datetime.datetime.now().year
+copyright = '2008-%s, Agendaless Consulting' % thisyear
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -484,7 +485,7 @@ epub_title = 'The Pyramid Web Application Development Framework, Version %s' \
              % release
 epub_author = 'Chris McDonough'
 epub_publisher = 'Agendaless Consulting'
-epub_copyright = '2008-%d' % datetime.datetime.now().year
+epub_copyright = '2008-%d' % thisyear
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.