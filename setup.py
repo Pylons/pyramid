0a842ca89ddda952d1c1ef49ec99dfa582742ab0
Chris McDonough <chrism@plope.com>
2013-07-18 17:44:08 -0400

    prep for 1.4.3

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.4.2',
+      version='1.4.3',
       description=('The Pyramid web application development framework, a '
                    'Pylons project'),
       long_description=README + '\n\n' +  CHANGES,
