a10a8f56d0c4efc232e3138e5d97eff06efd52d1
Chris McDonough <chrism@plope.com>
2013-10-14 16:35:45 +0200

    temporarily dont run tests to see impact on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -150,16 +150,16 @@ if 'sphinx-build' in ' '.join(sys.argv):  # protect against dumb importers
 
     sys.path.append(os.path.abspath('_themes'))
 
-    parent = os.path.dirname(os.path.dirname(__file__))
-    sys.path.append(os.path.abspath(parent))
-    wd = os.getcwd()
-    os.chdir(parent)
-    os.system('%s setup.py test -q' % sys.executable)
-    os.chdir(wd)
+    # parent = os.path.dirname(os.path.dirname(__file__))
+    # sys.path.append(os.path.abspath(parent))
+    # wd = os.getcwd()
+    # os.chdir(parent)
+    # os.system('%s setup.py test -q' % sys.executable)
+    # os.chdir(wd)
 
-    for item in os.listdir(parent):
-        if item.endswith('.egg'):
-            sys.path.append(os.path.join(parent, item))
+    # for item in os.listdir(parent):
+    #     if item.endswith('.egg'):
+    #         sys.path.append(os.path.join(parent, item))
 
 html_theme_path = ['_themes']
 html_theme = 'pyramid'
