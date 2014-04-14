196bcce7d623a2bddf5d515c5e32738067c46a4b
Michael Merickel <michael@merickel.org>
2014-04-14 14:10:23 -0400

    revert 1.5-branch version for rtd

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.5.1dev',
+      version='1.5',
       description='The Pyramid Web Framework, a Pylons project',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
