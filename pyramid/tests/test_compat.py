03a0d79306b2846313df1983a721d5cccf4ec3ce
John Anderson <sontek@gmail.com>
2014-12-26 23:19:32 -0800

    Clean up compat tests

--- pyramid/tests/test_compat.py
+++ pyramid/tests/test_compat.py
@@ -1,46 +1,26 @@
 import unittest
+from pyramid.compat import is_unbound_method
 
 class TestUnboundMethods(unittest.TestCase):
     def test_old_style_bound(self):
-        from pyramid.compat import is_unbound_method
-
-        class OldStyle:
-            def run(self):
-                return 'OK'
-
         self.assertFalse(is_unbound_method(OldStyle().run))
 
     def test_new_style_bound(self):
-        from pyramid.compat import is_unbound_method
-
-        class NewStyle(object):
-            def run(self):
-                return 'OK'
-
         self.assertFalse(is_unbound_method(NewStyle().run))
 
     def test_old_style_unbound(self):
-        from pyramid.compat import is_unbound_method
-
-        class OldStyle:
-            def run(self):
-                return 'OK'
-
         self.assertTrue(is_unbound_method(OldStyle.run))
 
     def test_new_style_unbound(self):
-        from pyramid.compat import is_unbound_method
-
-        class NewStyle(object):
-            def run(self):
-                return 'OK'
-
         self.assertTrue(is_unbound_method(NewStyle.run))
 
     def test_normal_func_unbound(self):
-        from pyramid.compat import is_unbound_method
-
-        def func():
-            return 'OK'
+        def func(): return 'OK'
 
         self.assertFalse(is_unbound_method(func))
+
+class OldStyle:
+    def run(self): return 'OK'
+
+class NewStyle(object):
+    def run(self): return 'OK'
