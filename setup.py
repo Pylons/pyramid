9779762c4afffab5cf6758fc4614e9e1eb49ad92
Chris McDonough <chrism@plope.com>
2015-04-14 21:22:19 -0400

    prep for 1.5.6

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.5.5',
+      version='1.5.6',
       description='The Pyramid Web Framework, a Pylons project',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
