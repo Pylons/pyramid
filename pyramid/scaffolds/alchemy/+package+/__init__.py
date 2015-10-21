c99c4b6c35cf73d57c738990652ae2eb2136ac02
Marcin Lulek <info@webreactor.eu>
2015-10-21 21:47:54 +0200

    alchemy: use jinja2 as default templating engine

--- pyramid/scaffolds/alchemy/+package+/__init__.py
+++ pyramid/scaffolds/alchemy/+package+/__init__.py
@@ -4,7 +4,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
     config.include('.models.meta')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
--- pyramid/scaffolds/alchemy/setup.py_tmpl
+++ pyramid/scaffolds/alchemy/setup.py_tmpl
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 
 requires = [
     'pyramid',
-    'pyramid_chameleon',
+    'pyramid_jinja2',
     'pyramid_debugtoolbar',
     'pyramid_tm',
     'SQLAlchemy',
