a56ca1961505bad79c7b9f9713e18e4b90f2f35a
Marcin Lulek <info@webreactor.eu>
2015-10-21 21:15:57 +0200

    alchemy: change file structure to modules for views and models

--- pyramid/scaffolds/alchemy/+package+/models/meta.py
+++ pyramid/scaffolds/alchemy/+package+/models/meta.py
@@ -1,10 +1,3 @@
-from sqlalchemy import (
-    Column,
-    Index,
-    Integer,
-    Text,
-    )
-
 from sqlalchemy import engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -52,11 +45,3 @@ def get_dbmaker(engine):
     return dbmaker
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
