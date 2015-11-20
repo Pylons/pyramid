de9ad0e53b892d62a6091bfee1d4363a0f596326
Patricio Paez <pp@pp.com.mx>
2015-11-20 07:07:20 -0600

    Update wiki2/tests for jinja2 templates
    - This is work in progress, do not pull yet
    - Functional tests do not pass yet, need define
      how to initialize the application's in-memory
      database

--- docs/tutorials/wiki2/src/tests/tutorial/views/default.py
+++ docs/tutorials/wiki2/src/tests/tutorial/views/default.py
@@ -1,3 +1,4 @@
+import cgi
 import re
 from docutils.core import publish_parts
 
@@ -16,13 +17,9 @@ from pyramid.security import (
     forget,
     )
 
-from .security import USERS
+from ..security.default import USERS
 
-from .models import (
-    DBSession,
-    Page,
-    )
-
+from ..models.mymodel import Page
 
 # regular expression used to find WikiWords
 wikiwords = re.compile(r"\b([A-Z]\w+[A-Z]+\w+)")
@@ -30,26 +27,26 @@ wikiwords = re.compile(r"\b([A-Z]\w+[A-Z]+\w+)")
 @view_config(route_name='view_wiki',
              permission='view')
 def view_wiki(request):
-    return HTTPFound(location = request.route_url('view_page',
-                                                  pagename='FrontPage'))
+    return HTTPFound(location=request.route_url('view_page',
+                                                pagename='FrontPage'))
 
-@view_config(route_name='view_page', renderer='templates/view.pt',
+@view_config(route_name='view_page', renderer='templates/view.jinja2',
              permission='view')
 def view_page(request):
     pagename = request.matchdict['pagename']
-    page = DBSession.query(Page).filter_by(name=pagename).first()
+    page = request.dbsession.query(Page).filter_by(name=pagename).first()
     if page is None:
         return HTTPNotFound('No such page')
 
     def check(match):
         word = match.group(1)
-        exists = DBSession.query(Page).filter_by(name=word).all()
+        exists = request.dbsession.query(Page).filter_by(name=word).all()
         if exists:
             view_url = request.route_url('view_page', pagename=word)
-            return '<a href="%s">%s</a>' % (view_url, word)
+            return '<a href="%s">%s</a>' % (view_url, cgi.escape(word))
         else:
             add_url = request.route_url('add_page', pagename=word)
-            return '<a href="%s">%s</a>' % (add_url, word)
+            return '<a href="%s">%s</a>' % (add_url, cgi.escape(word))
 
     content = publish_parts(page.data, writer_name='html')['html_body']
     content = wikiwords.sub(check, content)
@@ -57,14 +54,14 @@ def view_page(request):
     return dict(page=page, content=content, edit_url=edit_url,
                 logged_in=request.authenticated_userid)
 
-@view_config(route_name='add_page', renderer='templates/edit.pt',
+@view_config(route_name='add_page', renderer='templates/edit.jinja2',
              permission='edit')
 def add_page(request):
     pagename = request.matchdict['pagename']
     if 'form.submitted' in request.params:
         body = request.params['body']
         page = Page(name=pagename, data=body)
-        DBSession.add(page)
+        request.dbsession.add(page)
         return HTTPFound(location = request.route_url('view_page',
                                                       pagename=pagename))
     save_url = request.route_url('add_page', pagename=pagename)
@@ -72,24 +69,24 @@ def add_page(request):
     return dict(page=page, save_url=save_url,
                 logged_in=request.authenticated_userid)
 
-@view_config(route_name='edit_page', renderer='templates/edit.pt',
+@view_config(route_name='edit_page', renderer='templates/edit.jinja2',
              permission='edit')
 def edit_page(request):
     pagename = request.matchdict['pagename']
-    page = DBSession.query(Page).filter_by(name=pagename).one()
+    page = request.dbsession.query(Page).filter_by(name=pagename).one()
     if 'form.submitted' in request.params:
         page.data = request.params['body']
-        DBSession.add(page)
+        request.dbsession.add(page)
         return HTTPFound(location = request.route_url('view_page',
                                                       pagename=pagename))
     return dict(
         page=page,
-        save_url=request.route_url('edit_page', pagename=pagename),
+        save_url = request.route_url('edit_page', pagename=pagename),
         logged_in=request.authenticated_userid
         )
 
-@view_config(route_name='login', renderer='templates/login.pt')
-@forbidden_view_config(renderer='templates/login.pt')
+@view_config(route_name='login', renderer='templates/login.jinja2')
+@forbidden_view_config(renderer='templates/login.jinja2')
 def login(request):
     login_url = request.route_url('login')
     referrer = request.url
