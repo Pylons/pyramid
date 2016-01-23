5f8fd21e99f43b29dba3aea21546ba0e089861a0
Steve Piercy <web@stevepiercy.com>
2016-01-23 05:04:45 -0800

    Complete overhaul of Quick Tour - Databases and Forms (cherry picked from commit 6a93665)

--- docs/quick_tour/sqla_demo/sqla_demo/scripts/initializedb.py
+++ docs/quick_tour/sqla_demo/sqla_demo/scripts/initializedb.py
@@ -9,6 +9,8 @@ from pyramid.paster import (
     setup_logging,
     )
 
+from pyramid.scripts.common import parse_vars
+
 from ..models import (
     DBSession,
     MyModel,
@@ -18,17 +20,18 @@ from ..models import (
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
-    print('usage: %s <config_uri>\n'
+    print('usage: %s <config_uri> [var=value]\n'
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
 
 def main(argv=sys.argv):
-    if len(argv) != 2:
+    if len(argv) < 2:
         usage(argv)
     config_uri = argv[1]
+    options = parse_vars(argv[2:])
     setup_logging(config_uri)
-    settings = get_appsettings(config_uri)
+    settings = get_appsettings(config_uri, options=options)
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
--- docs/quick_tour/sqla_demo/sqla_demo/views.py
+++ docs/quick_tour/sqla_demo/sqla_demo/views.py
@@ -12,19 +12,18 @@ from .models import (
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     try:
-        # Start Sphinx Include
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-        # End Sphinx Include
     except DBAPIError:
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
     return {'one': one, 'project': 'sqla_demo'}
+
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
 
 1.  You may need to run the "initialize_sqla_demo_db" script
-    to initialize your database tables.  Check your virtual 
+    to initialize your database tables.  Check your virtual
     environment's "bin" directory for this script and try to run it.
 
 2.  Your database server may not be running.  Check that the
