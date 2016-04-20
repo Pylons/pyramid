947aca9cf28477c4ebb45e319ee2eaa638d4e0d9
Michael Merickel <michael@merickel.org>
2016-04-19 20:20:17 -0500

    remove vestiges of pyramid.require_default_csrf

--- pyramid/config/settings.py
+++ pyramid/config/settings.py
@@ -122,8 +122,6 @@ class Settings(dict):
                                              config_prevent_cachebust)
         eff_prevent_cachebust = asbool(eget('PYRAMID_PREVENT_CACHEBUST',
                                              config_prevent_cachebust))
-        require_default_csrf = self.get('pyramid.require_default_csrf')
-        eff_require_default_csrf = require_default_csrf
         csrf_trusted_origins = self.get("pyramid.csrf_trusted_origins", [])
         eff_csrf_trusted_origins = csrf_trusted_origins
 
@@ -138,7 +136,6 @@ class Settings(dict):
             'default_locale_name':eff_locale_name,
             'prevent_http_cache':eff_prevent_http_cache,
             'prevent_cachebust':eff_prevent_cachebust,
-            'require_default_csrf':eff_require_default_csrf,
             'csrf_trusted_origins':eff_csrf_trusted_origins,
 
             'pyramid.debug_authorization': eff_debug_all or eff_debug_auth,
@@ -151,7 +148,6 @@ class Settings(dict):
             'pyramid.default_locale_name':eff_locale_name,
             'pyramid.prevent_http_cache':eff_prevent_http_cache,
             'pyramid.prevent_cachebust':eff_prevent_cachebust,
-            'pyramid.require_default_csrf':eff_require_default_csrf,
             'pyramid.csrf_trusted_origins':eff_csrf_trusted_origins,
         }
 
