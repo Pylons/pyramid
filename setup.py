1821759e863e5453eeecbd40238b93923eaadff4
Michael Merickel <michael@merickel.org>
2016-05-19 22:25:41 -0500

    prep 1.7

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.7b4',
+      version='1.7',
       description='The Pyramid Web Framework, a Pylons project',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
