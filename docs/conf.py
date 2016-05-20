b029479301c61ac78e76e0ea3f184fd3c07a3c4e
Michael Merickel <michael@merickel.org>
2016-05-19 22:41:13 -0500

    enable docs to point to /latest

--- docs/conf.py
+++ docs/conf.py
@@ -55,8 +55,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
     'sphinxcontrib.programoutput',
-    # enable pylons_sphinx_latesturl when this branch is no longer "latest"
-    # 'pylons_sphinx_latesturl',
+    'pylons_sphinx_latesturl',
     ]
 
 # Looks for objects in external projects
@@ -126,21 +125,20 @@ if book:
 
 # Options for HTML output
 # -----------------------
-# enable pylons_sphinx_latesturl when this branch is no longer "latest"
-# pylons_sphinx_latesturl_base = (
-#     'http://docs.pylonsproject.org/projects/pyramid/en/latest/')
-# pylons_sphinx_latesturl_pagename_overrides = {
-#     # map old pagename -> new pagename
-#     'whatsnew-1.0': 'index',
-#     'whatsnew-1.1': 'index',
-#     'whatsnew-1.2': 'index',
-#     'whatsnew-1.3': 'index',
-#     'whatsnew-1.4': 'index',
-#     'whatsnew-1.5': 'index',
-#     'tutorials/gae/index': 'index',
-#     'api/chameleon_text': 'api',
-#     'api/chameleon_zpt': 'api',
-# }
+pylons_sphinx_latesturl_base = (
+    'http://docs.pylonsproject.org/projects/pyramid/en/latest/')
+pylons_sphinx_latesturl_pagename_overrides = {
+    # map old pagename -> new pagename
+    'whatsnew-1.0': 'index',
+    'whatsnew-1.1': 'index',
+    'whatsnew-1.2': 'index',
+    'whatsnew-1.3': 'index',
+    'whatsnew-1.4': 'index',
+    'whatsnew-1.5': 'index',
+    'tutorials/gae/index': 'index',
+    'api/chameleon_text': 'api',
+    'api/chameleon_zpt': 'api',
+}
 
 html_theme = 'pyramid'
 html_theme_path = pylons_sphinx_themes.get_html_themes_path()
