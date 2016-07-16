9e76dd908e7cb2028ce432b29e6211a1a9b503d6
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2016-07-16 16:49:05 +0800

    Add one-way password hash to security example in Wiki Tutorial.

--- docs/tutorials/wiki/src/authorization/tutorial/security.py
+++ docs/tutorials/wiki/src/authorization/tutorial/security.py
@@ -1,6 +1,24 @@
-USERS = {'editor':'editor',
-          'viewer':'viewer'}
-GROUPS = {'editor':['group:editors']}
+import hashlib
+import os
+import binascii
+
+SALT_LENGTH = 16
+
+
+def hash_password_with_salt(password, salt=None):
+    salt = salt or os.urandom(SALT_LENGTH)
+    dk = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+    return binascii.hexlify(dk + salt).decode('utf-8')
+
+def check_password(password, salted_password):
+    salt = binascii.unhexlify(salted_password[-2 * SALT_LENGTH:])
+    return hash_password_with_salt(password, salt) == salted_password
+
+
+USERS = {'editor': hash_password_with_salt('editor'),
+         'viewer': hash_password_with_salt('viewer')}
+GROUPS = {'editor': ['group:editors']}
+
 
 def groupfinder(userid, request):
     if userid in USERS:
