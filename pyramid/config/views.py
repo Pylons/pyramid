82dfca690b8bf0d93a678c86b9c3688ceb5262bd
Michael Merickel <michael@merickel.org>
2016-09-25 11:30:54 -0400

    clarify what changed in 1.6 with ``config.add_notfound_view``

--- pyramid/config/views.py
+++ pyramid/config/views.py
@@ -1544,6 +1544,10 @@ class ViewsConfiguratorMixin(object):
         be used` for the redirect response if a slash-appended route is found.
 
         .. versionchanged:: 1.6
+           The ``append_slash`` argument was modified to allow any object that
+           implements the ``IResponse`` interface to specify the response class
+           used when a redirect is performed.
+
         .. versionadded:: 1.3
         """
         for arg in (
