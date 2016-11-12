ece7e52ec48c8a0b41ba05d793f3fd803a654e9a
Moriyoshi Koizumi <mozo@mozo.jp>
2016-11-12 16:42:32 +0900

    Mark a few more characters as safe when escaping path components in route path generation, according to pyramid.url.PATH_SAFE

--- pyramid/urldispatch.py
+++ pyramid/urldispatch.py
@@ -207,6 +207,10 @@ def _compile_route(route):
         return d
 
     gen = ''.join(gen)
+
+    def q(v):
+        return quote_path_segment(v, safe='/:@&+$,')
+
     def generator(dict):
         newdict = {}
         for k, v in dict.items():
@@ -223,17 +227,17 @@ def _compile_route(route):
                 # a stararg argument
                 if is_nonstr_iter(v):
                     v = '/'.join(
-                        [quote_path_segment(x, safe='/') for x in v]
+                        [q(x) for x in v]
                         ) # native
                 else:
                     if v.__class__ not in string_types:
                         v = str(v)
-                    v = quote_path_segment(v, safe='/')
+                    v = q(v)
             else:
                 if v.__class__ not in string_types:
                     v = str(v)
                 # v may be bytes (py2) or native string (py3)
-                v = quote_path_segment(v, safe='/')
+                v = q(v)
 
             # at this point, the value will be a native string
             newdict[k] = v
