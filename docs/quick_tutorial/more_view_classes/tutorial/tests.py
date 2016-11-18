aa61da3e81426b97ca3f1699acc40e2837448846
Terrance Kennedy <terrancerkennedy@gmail.com>
2016-11-18 00:16:09 -0700

    increase more_view_classes test coverage to 100%

--- docs/quick_tutorial/more_view_classes/tutorial/tests.py
+++ docs/quick_tutorial/more_view_classes/tutorial/tests.py
@@ -10,6 +10,15 @@ class TutorialViewTests(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
+    def test_full_name(self):
+        from .views import TutorialViews
+
+        request = testing.DummyRequest()
+        request.matchdict['first'] = 'jane'
+        request.matchdict['last'] = 'doe'
+        inst = TutorialViews(request)
+        self.assertEqual('jane doe', inst.full_name)
+
     def test_home(self):
         from .views import TutorialViews
 
@@ -17,6 +26,32 @@ class TutorialViewTests(unittest.TestCase):
         inst = TutorialViews(request)
         response = inst.home()
         self.assertEqual('Home View', response['page_title'])
+
+    def test_hello(self):
+        from .views import TutorialViews
+
+        request = testing.DummyRequest()
+        inst = TutorialViews(request)
+        response = inst.hello()
+        self.assertEqual('Hello View', response['page_title'])
+
+    def test_edit(self):
+        from .views import TutorialViews
+
+        request = testing.DummyRequest(params={'new_name': 'John Doe'})
+        inst = TutorialViews(request)
+        response = inst.edit()
+        self.assertEqual('Edit View', response['page_title'])
+        self.assertEqual('John Doe', response['new_name'])
+
+    def test_delete(self):
+        from .views import TutorialViews
+
+        request = testing.DummyRequest()
+        inst = TutorialViews(request)
+        response = inst.delete()
+        self.assertEqual('Delete View', response['page_title'])
+
 
 class TutorialFunctionalTests(unittest.TestCase):
     def setUp(self):
@@ -29,3 +64,18 @@ class TutorialFunctionalTests(unittest.TestCase):
     def test_home(self):
         res = self.testapp.get('/', status=200)
         self.assertIn(b'TutorialViews - Home View', res.body)
+
+    def test_hello(self):
+        res = self.testapp.get('/howdy/jane/doe', status=200)
+        self.assertIn(b'TutorialViews - Hello View', res.body)
+
+    def test_edit(self):
+        res = self.testapp.post(
+                '/howdy/jane/doe', params={'new_name': 'John Doe'}, status=200)
+        self.assertIn(b'TutorialViews - Edit View', res.body)
+        self.assertIn(b'John Doe', res.body)
+
+    def test_delete(self):
+        res = self.testapp.post(
+                '/howdy/jane/doe', params={'form.delete': 'Jane Doe'}, status=200)
+        self.assertIn(b'TutorialViews - Delete View', res.body)
