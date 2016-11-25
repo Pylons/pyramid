bf5c9eaa7e9041fe76cafddf7bad0f397899a6e9
Steve Piercy <web@stevepiercy.com>
2016-11-25 14:58:20 -0800

    add todo Sphinx extension for todo blocks - add lists, tables, danger, todo, comments

--- docs/conf.py
+++ docs/conf.py
@@ -51,9 +51,10 @@ book = os.environ.get('BOOK')
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'repoze.sphinx.autointerface',
-    'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'repoze.sphinx.autointerface',
     'sphinxcontrib.programoutput',
     # enable pylons_sphinx_latesturl when this branch is no longer "latest"
     # 'pylons_sphinx_latesturl',
@@ -119,6 +120,9 @@ exclude_patterns = ['_themes/README.rst', ]
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
 add_module_names = False
+
+# Add support for todo items
+todo_include_todos = True
 
 # The name of the Pygments (syntax highlighting) style to use.
 #pygments_style = book and 'bw' or 'tango'
