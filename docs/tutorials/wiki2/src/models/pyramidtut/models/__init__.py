8ded41edfc62578bac0940abf1dafdb0b5ede1b2
Steve Piercy <web@stevepiercy.com>
2016-12-20 02:53:52 -0800

    wiki2/definingmodels - update src files for cookiecutter

--- docs/tutorials/wiki2/src/models/pyramidtut/models/__init__.py
+++ docs/tutorials/wiki2/src/models/pyramidtut/models/__init__.py
@@ -5,7 +5,8 @@ import zope.sqlalchemy
 
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
-from .mymodel import MyModel # flake8: noqa
+from .page import Page  # noqa
+from .user import User  # noqa
 
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
--- docs/tutorials/wiki2/src/models/pyramidtut/scripts/initializedb.py
+++ docs/tutorials/wiki2/src/models/pyramidtut/scripts/initializedb.py
@@ -15,7 +15,7 @@ from ..models import (
     get_session_factory,
     get_tm_session,
     )
-from ..models import MyModel
+from ..models import Page, User
 
 
 def usage(argv):
@@ -41,5 +41,17 @@ def main(argv=sys.argv):
     with transaction.manager:
         dbsession = get_tm_session(session_factory, transaction.manager)
 
-        model = MyModel(name='one', value=1)
-        dbsession.add(model)
+        editor = User(name='editor', role='editor')
+        editor.set_password('editor')
+        dbsession.add(editor)
+
+        basic = User(name='basic', role='basic')
+        basic.set_password('basic')
+        dbsession.add(basic)
+
+        page = Page(
+            name='FrontPage',
+            creator=editor,
+            data='This is the front page',
+        )
+        dbsession.add(page)
