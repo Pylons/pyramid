b38bbfb1b64a38e04490aaf3ed061067968c42f5
Michael Merickel <michael@merickel.org>
2017-01-21 20:11:12 -0600

    switch to 1.9.dev0

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.8b1',
+      version='1.9.dev0',
       description='The Pyramid Web Framework, a Pylons project',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
