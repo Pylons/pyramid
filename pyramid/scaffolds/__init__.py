6c38f2b732cf2806bbd28efe77491ee0fe157855
Steve Piercy <web@stevepiercy.com>
2017-03-12 10:33:24 -0700

    update twitter handle

--- pyramid/scaffolds/__init__.py
+++ pyramid/scaffolds/__init__.py
@@ -37,7 +37,7 @@ class PyramidTemplate(Template):
             %(separator)s
             Tutorials:     http://docs.pylonsproject.org/projects/pyramid_tutorials/en/latest/
             Documentation: http://docs.pylonsproject.org/projects/pyramid/en/latest/
-            Twitter:       https://twitter.com/trypyramid
+            Twitter:       https://twitter.com/PylonsProject
             Mailing List:  https://groups.google.com/forum/#!forum/pylons-discuss
 
             Welcome to Pyramid.  Sorry for the convenience.
