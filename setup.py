8b96cfeedd5aee88cfb1a744d480130853526a87
Michael Merickel <michael@merickel.org>
2017-03-28 01:10:36 -0500

    add python_requires metadata

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(name='pyramid',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
       install_requires=install_requires,
       extras_require={
           'testing': testing_extras,
