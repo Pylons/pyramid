e33212c6746cbf50c4952b5d8c8d93714cf06ccd
Steve Piercy <web@stevepiercy.com>
2017-07-03 21:06:56 -0700

    Update example apps to use config context manager in Quick Tour - add missing EOF line ending

--- docs/quick_tour/requests/app.py
+++ docs/quick_tour/requests/app.py
@@ -16,9 +16,9 @@ def hello_world(request):
 
 
 if __name__ == '__main__':
-    config = Configurator()
-    config.add_route('hello', '/')
-    config.add_view(hello_world, route_name='hello')
-    app = config.make_wsgi_app()
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
