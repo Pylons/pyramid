29615272e0f5668373f5e1eca56d3209cfac8ffa
Michael Merickel <michael@merickel.org>
2017-07-13 22:33:35 -0500

    prep 1.8.5

--- docs/whatsnew-1.8.rst
+++ docs/whatsnew-1.8.rst
@@ -19,6 +19,7 @@ include documentation improvements and other minor feature changes.
 - :ref:`changes_1.8.2`
 - :ref:`changes_1.8.3`
 - :ref:`changes_1.8.4`
+- :ref:`changes_1.8.5`
 
 Major Feature Additions
 -----------------------
--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='pyramid',
-      version='1.8.4',
+      version='1.8.5',
       description='The Pyramid Web Framework, a Pylons project',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
