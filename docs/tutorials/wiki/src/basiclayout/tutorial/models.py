1ed4cc20651537b903609b42f5b35e1a98441a16
Steve Piercy <web@stevepiercy.com>
2017-07-27 00:42:01 -0700

    update zodb wiki files to synch with its cookiecutter
    (cherry picked from commit c1e805b)

--- docs/tutorials/wiki/src/basiclayout/tutorial/models.py
+++ docs/tutorials/wiki/src/basiclayout/tutorial/models.py
@@ -9,6 +9,4 @@ def appmaker(zodb_root):
     if 'app_root' not in zodb_root:
         app_root = MyModel()
         zodb_root['app_root'] = app_root
-        import transaction
-        transaction.commit()
     return zodb_root['app_root']
