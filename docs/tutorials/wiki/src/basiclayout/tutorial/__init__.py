759febef4df499d7dbd68e0a74f17b69e4e68ad2
Steve Piercy <web@stevepiercy.com>
2017-08-03 22:36:25 -0700

    update zodb wiki files to synch with its cookiecutter using context manager

--- docs/tutorials/wiki/src/basiclayout/tutorial/__init__.py
+++ docs/tutorials/wiki/src/basiclayout/tutorial/__init__.py
@@ -11,13 +11,13 @@ def root_factory(request):
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(root_factory=root_factory, settings=settings)
-    settings = config.get_settings()
     settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
-    config.include('pyramid_chameleon')
-    config.include('pyramid_tm')
-    config.include('pyramid_retry')
-    config.include('pyramid_zodbconn')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.scan()
-    return config.make_wsgi_app()
+    with Configurator(settings=settings) as config:
+        config.include('pyramid_chameleon')
+        config.include('pyramid_tm')
+        config.include('pyramid_retry')
+        config.include('pyramid_zodbconn')
+        config.set_root_factory(root_factory)
+        config.add_static_view('static', 'static', cache_max_age=3600)
+        config.scan()
+        return config.make_wsgi_app()
