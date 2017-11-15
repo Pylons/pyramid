f32dac8e3a33b463a40abc447a7f9e318e38183a
John Wu <webmaster@leapoahead.com>
2017-11-15 14:43:54 -0800

    Clarify property descriptor in add_request_method
    Fix #3202 - Doc change.

--- pyramid/config/factories.py
+++ pyramid/config/factories.py
@@ -144,9 +144,10 @@ class FactoriesConfiguratorMixin(object):
 
         When adding a property to the request, ``callable`` can either
         be a callable that accepts the request as its single positional
-        parameter, or it can be a property descriptor. If ``name`` is
-        ``None``, the name of the property will be computed from the
-        name of the ``callable``.
+        parameter, or it can be a property descriptor. If ``callable`` is
+        a property descriptor, it has to be a instance of a class which is
+        a subclass of ``property``. If ``name`` is ``None``, the name of
+        the property will be computed from the name of the ``callable``.
 
         If the ``callable`` is a property descriptor a ``ValueError``
         will be raised if ``name`` is ``None`` or ``reify`` is ``True``.
