8b43919a98177d4591736c3125b79ab0eeed290f
Steve Piercy <web@stevepiercy.com>
2018-06-22 18:09:59 -0700

    Update source files for basiclayout step for Alembic support

--- docs/tutorials/wiki2/src/basiclayout/setup.py
+++ docs/tutorials/wiki2/src/basiclayout/setup.py
@@ -9,6 +9,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
+    'alembic',
     'plaster_pastedeploy',
     'pyramid >= 1.9a',
     'pyramid_debugtoolbar',
@@ -54,7 +55,7 @@ setup(
             'main = tutorial:main',
         ],
         'console_scripts': [
-            'initialize_tutorial_db = tutorial.scripts.initializedb:main',
+            'initialize_tutorial_db = tutorial.scripts.initialize_db:main',
         ],
     },
 )
--- docs/tutorials/wiki2/src/basiclayout/tutorial/views/default.py
+++ docs/tutorials/wiki2/src/basiclayout/tutorial/views/default.py
@@ -20,9 +20,8 @@ db_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
 
-1.  You may need to run the "initialize_tutorial_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
+1.  You may need to initialize your database tables with `alembic`.
+    Check your README.txt for description and try to run it.
 
 2.  Your database server may not be running.  Check that the
     database server referred to by the "sqlalchemy.url" setting in
