178bf2077247ff8e88f21e112b082fe0a375020b
Steve Piercy <web@stevepiercy.com>
2018-06-29 00:41:04 -0700

    More synching of src to cc files

--- docs/tutorials/wiki2/src/installation/tutorial/views/default.py
+++ docs/tutorials/wiki2/src/installation/tutorial/views/default.py
@@ -3,14 +3,14 @@ from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import MyModel
+from .. import models
 
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
     try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
+        query = request.dbsession.query(models.MyModel)
+        one = query.filter(models.MyModel.name == 'one').first()
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
     return {'one': one, 'project': 'myproj'}
