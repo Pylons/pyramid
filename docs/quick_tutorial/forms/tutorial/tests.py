75ee95b56bcaca95cbda08a1cf2de8e434f7c29a
Will <williamtpereira@gmail.com>
2018-10-06 12:25:46 -0300

    add new tests in the forms and validation in quick tutorial

--- docs/quick_tutorial/forms/tutorial/tests.py
+++ docs/quick_tutorial/forms/tutorial/tests.py
@@ -34,3 +34,33 @@ class TutorialFunctionalTests(unittest.TestCase):
     def test_home(self):
         res = self.testapp.get('/', status=200)
         self.assertIn(b'<title>Wiki: View</title>', res.body)
+
+    def test_add_page(self):
+        res = self.testapp.get('/add', status=200)
+        self.assertIn(b'<h1>Wiki</h1>', res.body)
+
+    def test_edit_page(self):
+        res = self.testapp.get('/101/edit', status=200)
+        self.assertIn(b'<h1>Wiki</h1>', res.body)
+
+    def test_post_wiki(self):
+        self.testapp.post('/add', {
+            "title": "New Title",
+            "body": "<p>New Body</p>",
+            "submit": "submit"
+        }, status=302)
+
+        res = self.testapp.get('/103', status=200)
+        self.assertIn(b'<h1>New Title</h1>', res.body)
+        self.assertIn(b'<p>New Body</p>', res.body)
+
+    def test_edit_wiki(self):
+        self.testapp.post('/102/edit', {
+            "title": "New Title",
+            "body": "<p>New Body</p>",
+            "submit": "submit"
+        }, status=302)
+
+        res = self.testapp.get('/102', status=200)
+        self.assertIn(b'<h1>New Title</h1>', res.body)
+        self.assertIn(b'<p>New Body</p>', res.body)
