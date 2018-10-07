18435c3ac41d143c0f42062c095a638f522a30dc
Steve Piercy <web@stevepiercy.com>
2018-10-07 04:00:19 -0700

    Add pyramid_jinja2 as a requirement. Carry forward previous setup.py changes.

--- docs/quick_tutorial/jinja2/setup.py
+++ docs/quick_tutorial/jinja2/setup.py
@@ -2,13 +2,22 @@ from setuptools import setup
 
 requires = [
     'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_jinja2',
     'waitress',
 ]
 
 setup(name='tutorial',
       install_requires=requires,
+      extras_require={
+          'test': [
+              'pytest',
+              'webtest',
+          ],
+      },
       entry_points="""\
       [paste.app_factory]
       main = tutorial:main
       """,
-)
+      )
