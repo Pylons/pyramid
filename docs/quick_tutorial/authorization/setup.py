a842f3ac4eda7d94537c2737de19f9d0ea269008
Steve Piercy <web@stevepiercy.com>
2018-10-07 04:25:01 -0700

    Carry forward previous setup.py changes.

--- docs/quick_tutorial/authorization/setup.py
+++ docs/quick_tutorial/authorization/setup.py
@@ -4,13 +4,20 @@ requires = [
     'bcrypt',
     'pyramid',
     'pyramid_chameleon',
+    'pyramid_debugtoolbar',
     'waitress',
 ]
 
 setup(name='tutorial',
       install_requires=requires,
+      extras_require={
+          'test': [
+              'pytest',
+              'webtest',
+          ],
+      },
       entry_points="""\
       [paste.app_factory]
       main = tutorial:main
       """,
-)
+      )
