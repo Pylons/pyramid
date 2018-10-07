4e1175a5d701afdc98c09fd4dfe0b90db00a4e3a
Steve Piercy <web@stevepiercy.com>
2018-10-07 03:59:53 -0700

    Carry forward previous setup.py changes.

--- docs/quick_tutorial/request_response/setup.py
+++ docs/quick_tutorial/request_response/setup.py
@@ -2,13 +2,21 @@ from setuptools import setup
 
 requires = [
     'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
     'waitress',
 ]
 
 setup(name='tutorial',
       install_requires=requires,
+      extras_require={
+          'test': [
+              'pytest',
+              'webtest',
+          ],
+      },
       entry_points="""\
       [paste.app_factory]
       main = tutorial:main
       """,
-)
+      )
