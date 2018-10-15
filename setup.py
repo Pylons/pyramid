f9da0b676f29444b7d3628d0340b081fb02d4aa2
Michael Merickel <michael@merickel.org>
2018-10-14 23:17:20 -0500

    fix tests to run from the tests folder

--- setup.py
+++ setup.py
@@ -81,7 +81,8 @@ setup(name='pyramid',
       author_email="pylons-discuss@googlegroups.com",
       url="https://trypyramid.com",
       license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
-      packages=find_packages(),
+      packages=find_packages('src', exclude=['tests']),
+      package_dir={'': 'src'},
       include_package_data=True,
       zip_safe=False,
       python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
@@ -92,7 +93,7 @@ setup(name='pyramid',
           'docs': docs_extras,
       },
       tests_require=tests_require,
-      test_suite="pyramid.tests",
+      test_suite="tests",
       entry_points="""\
         [pyramid.scaffold]
         starter=pyramid.scaffolds:StarterProjectTemplate
