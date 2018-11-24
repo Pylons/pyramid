f326582294868e5529320b532738c8762cf816c5
Steve Piercy <web@stevepiercy.com>
2018-11-23 17:00:31 -0800

    Add files from cookiecutter

---
+++ docs/tutorials/wiki/src/installation/tutorial/models/__init__.py
@@ -0,0 +1,12 @@
+from persistent.mapping import PersistentMapping
+
+
+class MyModel(PersistentMapping):
+    __parent__ = __name__ = None
+
+
+def appmaker(zodb_root):
+    if 'app_root' not in zodb_root:
+        app_root = MyModel()
+        zodb_root['app_root'] = app_root
+    return zodb_root['app_root']
