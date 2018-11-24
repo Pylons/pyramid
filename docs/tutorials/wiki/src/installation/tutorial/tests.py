76ada71ec1d28d8f38f15dcffa311180d99d2daa
Steve Piercy <web@stevepiercy.com>
2018-11-23 16:59:32 -0800

    synch up source files with actual cookiecutter results

--- docs/tutorials/wiki/src/installation/tutorial/tests.py
+++ docs/tutorials/wiki/src/installation/tutorial/tests.py
@@ -11,7 +11,8 @@ class ViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_my_view(self):
-        from .views import my_view
+        from .views.default import my_view
         request = testing.DummyRequest()
         info = my_view(request)
         self.assertEqual(info['project'], 'myproj')
+
