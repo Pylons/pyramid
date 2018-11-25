622a98df9f46805617481c573073901493e601c4
Steve Piercy <web@stevepiercy.com>
2018-11-25 12:26:58 -0800

    Use path for pagename, not subpath

--- docs/tutorials/wiki/src/views/tutorial/views/notfound.py
+++ docs/tutorials/wiki/src/views/tutorial/views/notfound.py
@@ -6,7 +6,7 @@ from ..models import Page
 @notfound_view_config(renderer='../templates/404.pt')
 def notfound_view(request):
     request.response.status = 404
-    pagename = request.subpath
+    pagename = request.path
     page = Page(pagename)
     page.__name__ = pagename
     return dict(page=page)
