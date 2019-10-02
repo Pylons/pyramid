dc8f7ee13f7619f05632968b50bd64d9c19946fd
Michael Merickel <michael@merickel.org>
2019-10-01 19:18:41 -0500

    simplify caller_package logic

--- src/pyramid/path.py
+++ src/pyramid/path.py
@@ -60,13 +60,9 @@ def package_of(pkg_or_module):
 def caller_package(level=2, caller_module=caller_module):
     # caller_module in arglist for tests
     module = caller_module(level + 1)
-    f = getattr(module, '__file__', '')
-    if ('__init__.py' in f) or ('__init__$py' in f):  # empty at >>>
-        # Module is a package
+    if module.__package__ is None or module.__package__ == module.__name__:
         return module
-    # Go up one level to get package
-    package_name = module.__name__.rsplit('.', 1)[0]
-    return sys.modules[package_name]
+    return sys.modules[module.__package__]
 
 
 def package_path(package):
