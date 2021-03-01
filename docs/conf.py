4f4ebf7ae5b49ebfbddd72f3a4f28c434283dd27
Michael Merickel <michael@merickel.org>
2021-02-28 21:14:04 -0600

    mark docs outdated

--- docs/conf.py
+++ docs/conf.py
@@ -126,25 +126,25 @@ if book:
 # Options for HTML output
 # -----------------------
 # enable pylons_sphinx_latesturl when this branch is no longer "latest"
-# pylons_sphinx_latesturl_base = (
-#     'https://docs.pylonsproject.org/projects/pyramid/en/latest/')
-# pylons_sphinx_latesturl_pagename_overrides = {
-#     # map old pagename -> new pagename
-#     'whatsnew-1.0': 'index',
-#     'whatsnew-1.1': 'index',
-#     'whatsnew-1.2': 'index',
-#     'whatsnew-1.3': 'index',
-#     'whatsnew-1.4': 'index',
-#     'whatsnew-1.5': 'index',
-#     'whatsnew-1.6': 'index',
-#     'whatsnew-1.7': 'index',
-#     'whatsnew-1.8': 'index',
-#     'whatsnew-1.9': 'index',
-#     'whatsnew-1.10': 'index',
-#     'tutorials/gae/index': 'index',
-#     'api/chameleon_text': 'api',
-#     'api/chameleon_zpt': 'api',
-# }
+pylons_sphinx_latesturl_base = (
+    'https://docs.pylonsproject.org/projects/pyramid/en/latest/')
+pylons_sphinx_latesturl_pagename_overrides = {
+    # map old pagename -> new pagename
+    'whatsnew-1.0': 'index',
+    'whatsnew-1.1': 'index',
+    'whatsnew-1.2': 'index',
+    'whatsnew-1.3': 'index',
+    'whatsnew-1.4': 'index',
+    'whatsnew-1.5': 'index',
+    'whatsnew-1.6': 'index',
+    'whatsnew-1.7': 'index',
+    'whatsnew-1.8': 'index',
+    'whatsnew-1.9': 'index',
+    'whatsnew-1.10': 'index',
+    'tutorials/gae/index': 'index',
+    'api/chameleon_text': 'api',
+    'api/chameleon_zpt': 'api',
+}
 
 html_theme = 'pyramid'
 html_theme_path = pylons_sphinx_themes.get_html_themes_path()
@@ -154,7 +154,7 @@ html_theme_options = dict(
     # pre-release status: true; else: false.
     in_progress='false',
     # On branches previous to "latest": true; else: false.
-    outdated='false',
+    outdated='true',
     )
 
 # Control display of sidebars
