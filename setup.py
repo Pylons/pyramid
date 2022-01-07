c7250ee947d2804a54a272373758150cce9db539
Marcin Lulek <info@webreactor.eu>
2022-01-07 14:19:58 +0100

    Remove Python 3.6 tests

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
