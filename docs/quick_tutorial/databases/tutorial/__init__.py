a254f1d7bd23345d477d21bf83d201f9622ba835
Nguyễn Hoàng Anh Duy <me@duynha.dev>
2024-12-06 09:54:38 +0700

    Remove the usage of deprecated 'sqlalchemy.MetaData.bind' attribute

--- docs/quick_tutorial/databases/tutorial/__init__.py
+++ docs/quick_tutorial/databases/tutorial/__init__.py
@@ -7,7 +7,6 @@ from .models import DBSession, Base
 def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
 
     config = Configurator(settings=settings,
                           root_factory='tutorial.models.Root')
